3e7f4d13911cd3af4100927d2e6d801823c56a4d
Angiello Rivas <angiello@hotmail.com>
2019-08-28 13:15:31 -0500

    Create RotateViewPageTransformer.kt

---
+++ RotateViewPageTransformer.kt
@@ -0,0 +1,24 @@
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.viewpager2.widget.ViewPager2
+
+
+class PageTransformer : ViewPager2.PageTransformer {
+
+    override fun transformPage(view: View, position: Float) {
+
+        when {
+            position <= 1 -> {
+                    //REPLACE WITH YOUR VIEW ID
+                val image = view.findViewById<AppCompatImageView>(R.id.image)
+                image.rotation = position * 40F
+                view.alpha = 1F
+                view.pivotY = view.height * 0.5f
+                view.rotationY = 13f * position
+            }
+            else -> {
+                view.alpha = 0F
+            }
+        }
+    }
+}
